d7383ac9759d1aa5e2446cb24a93fcd4b2945deb
mary-ue <maryandreeva100@gmail.com>
2023-12-19 00:11:43 +0300

    add filters

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { JobPosition } from './JobPosition';
-import { selectAllPositions } from 'store/positions/position-selectors';
+import { selectVisiblePositions } from 'store/positions/position-selectors';
+import { addFilter } from 'store/filters/filter-actions';
+import { selectFilters } from 'store/filters/filter-selectors';
 
 const JobList = () => {
-  const positions = useSelector(selectAllPositions);
+  const dispatch = useDispatch();
+  const currentFitlers = useSelector(selectFilters);
+  const positions = useSelector((state) => selectVisiblePositions(state, currentFitlers));
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
 
   return (
     <div className='job-list'>
       {positions.map(item => (
-        <JobPosition key={item.id} {...item} />
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
       ))}
     </div>
   )
