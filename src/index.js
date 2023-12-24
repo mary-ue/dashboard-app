23bd500608ddcb089ae5e959e9aa633637070c72
mary-ue <maryandreeva100@gmail.com>
2023-12-24 19:03:04 +0300

    add lib redux-persist

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from 'store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    {/* loading - компонент-прелоадер */}
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
